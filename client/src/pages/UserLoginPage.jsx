11db8a420489ecf4f36bfd563ccf62ed90e73e73
steve2700 <stevnyaru@github.com>
2024-03-30 13:42:42 +0200

    intergrate the forgot password w the backend

--- client/src/pages/UserLoginPage.jsx
+++ client/src/pages/UserLoginPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import UserLoginForm from '../components/User/UserLoginForm';
+
 const UserLoginPage = () => {
   return (
-    <div>
-      <h1>User Login</h1>
+    <div className="login-container bg-gray-200">
+      <h1 className="text-3xl font-bold text-center mb-6">User Login</h1>
       <UserLoginForm />
     </div>
   );
--- client/src/pages/UserRegistrationPage.jsx
+++ client/src/pages/UserRegistrationPage.jsx
@@ -3,7 +3,15 @@ import UserRegistrationForm from '../components/User/UserRegistrationForm';
 
 const UserRegistrationPage = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+    <div className="registration-container bg-gray-200"> {/* Set background color to gray */}
+      {/* Colorful Title */}
+      <h1 className="text-3xl font-bold text-center mb-6">
+        <span className="text-red-500">J</span>
+        <span className="text-blue-500">o</span>
+        <span className="text-green-500">i</span>
+        <span className="text-yellow-500">n</span> our Community
+      </h1>
+
       <UserRegistrationForm />
     </div>
   );
